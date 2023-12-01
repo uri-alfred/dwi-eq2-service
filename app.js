db2e9319f444154633cb16021af1fdc397f7fa63
zzALFREDzz\Uriel <urielbotelloresendiz@gmail.com>
2023-12-01 17:16:42 -0600

    logs debug cookie

--- app.js
+++ app.js
@@ -42,20 +42,21 @@ let token = "";
 function verificarToken(req, res, next) {
     // Obtener el token de la cookie o de donde lo estés enviando
     
+    console.log("cookies",req.cookies)
     if (!req.cookies['auth-token']) {
         token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiY2VzYXJPcnRpekBnbWFpbC5jb20iLCJpYXQiOjE3MDExNDQxMjYsImV4cCI6MTcwMTMxNjkyNn0.JZbvmONwus1XO6Bmz1qDosQwLrTG5kAYw3EFNUrN-3DDRU-DBciXI-2BW54vsutTuSMWXGJLB2IB1O6DR0MKLKNXiAOISCG3IL56ZsIWT6haPQ-EXO7UC5qA7ubSagUCTgiM_leNK66wbCy42U6GwFF977IZrt8B0obmTe-9MrCmb9r3GfMRIZEa7ts6TFgzU7a_8FRb8pt0GwSHfOi8dT43Mh3ea_99h9EmcMZfCe4WGfxq4C5S10hbc5G7SaehlKBEx4Y9iFpZ74KuSs9egM8rvZHKm51rrVXIdZkIzI0-b3WAvlVjXeCPZxo31ZI1-1oYA8omxi7vF6y_VxUrig'
     } else {
         token = req.cookies['auth-token'];
-        // console.log(req.cookies)
     }
-  
+    
     // Verificar si el token existe
     if (!token) {
       return res.status(401).json({ error: 'Acceso no autorizado. Token no proporcionado.' });
     }
     // Verificar si el token es valido
     // Verificar el token
-    // console.log(token)
+    console.log("token",token)
+    console.log("clave_privada",clave_privada)
     jwt.verify(token, clave_privada, (err, decoded) => {
       if (err) {
         return res.status(401).json({ error: 'Acceso no autorizado. Token inválido.' });
